6b03b2d411448f1cbafebc8d3ca0de7ca761ef8f
davirolim <davirolimr94@gmail.com>
2021-02-18 20:28:04 +0000

    adding the http

--- my-app/src/services/UserService.js
+++ my-app/src/services/UserService.js
@@ -1,11 +1,11 @@
 export async function getAllUsers() {
-    const url = `api.addlist.local/api/users`
+    const url = `http://api.addlist.local/api/users`
     const response = await fetch(url);
     return await response.json();
 }
 
 export async function createUser(data) {
-    const url = `api.addlist.local/api/user`
+    const url = `http://api.addlist.local/api/user`
     const response = await fetch(url, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
