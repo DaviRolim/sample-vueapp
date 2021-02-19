5665695701b69d9d4d3a0f66fb0f3cc3ad2c5285
davirolim <davirolimr94@gmail.com>
2021-02-19 12:48:02 +0000

    adding axios

--- my-app/src/services/UserService.js
+++ my-app/src/services/UserService.js
@@ -1,11 +1,11 @@
+const axios = require('axios')
 export async function getAllUsers() {
-    const response = await fetch(`http://api.addlist.local:3000/api/users`);
+    const response = await axios.get(`http://api.addlist.local:3000/api/users`);
     return await response.json();
 }
 
 export async function createUser(data) {
-    const response = await fetch(`http://api.addlist.local:3000/api/user`, {
-        method: 'POST',
+    const response = await axios.post(`http://api.addlist.local:3000/api/user`, {
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({user: data})
       })
