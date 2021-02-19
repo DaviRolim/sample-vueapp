4db4e06fe1899f793cd1dd1666e64cdebd540234
davirolim <davirolimr94@gmail.com>
2021-02-19 12:18:45 +0000

    making call on port 3000

--- my-app/src/services/UserService.js
+++ my-app/src/services/UserService.js
@@ -1,10 +1,10 @@
 export async function getAllUsers() {
-    const response = await fetch(`http://api.addlist.local:80/api/users`);
+    const response = await fetch(`http://api.addlist.local:3000/api/users`);
     return await response.json();
 }
 
 export async function createUser(data) {
-    const response = await fetch(`http://api.addlist.local:80/api/user`, {
+    const response = await fetch(`http://api.addlist.local:3000/api/user`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({user: data})
